d7cb319d8e9b51cad60574b914a99cc1b1da0cb2
sherikovic <sherif.amer01@gmail.com>
2023-08-29 15:08:46 +0200

    .

---
+++ mongoose_express/seeds.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose
+	.connect('mongodb://127.0.0.1:27017/farmStand')
+	.then(console.log('mongo connection open!'))
+	.catch((err) => {
+		console.log('mongo connection failed!');
+		console.log(err);
+	});
+
+// const p = new Product({
+// 	name: 'Ruby Grapefruit',
+// 	price: 1.99,
+// 	category: 'fruit',
+// });
+
+// p.save()
+// 	.then((p) => console.log(p))
+// 	.catch((err) => console.log(err));
+
+const seedProducts = [
+	{
+		name: 'Fairy Eggplant',
+		price: 1.0,
+		category: 'vegetable',
+	},
+	{
+		name: 'Organic Goddess Melon',
+		price: 4.99,
+		category: 'fruit',
+	},
+	{
+		name: 'Organic Mini Seedless Watermelon',
+		price: 3.99,
+		category: 'fruit',
+	},
+	{
+		name: 'Organic Celery',
+		price: 1.5,
+		category: 'vegetable',
+	},
+	{
+		name: 'Chocolate Whole Milk',
+		price: 2.69,
+		category: 'dairy',
+	},
+];
+
+Product.insertMany(seedProducts)
+	.then((res) => console.log(res))
+	.catch((err) => console.log(err));
